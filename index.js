68859f48ac42c712ff8aab1059372b78ab9f7294
Sharm-Dev <sharmainetaylor@ymail.com>
2022-01-13 22:16:28 +0000

    add a variable in save function that holds the count from the count variable and also  attached a dash after it, also brought in the save-el from the html, and used the save function to add the innertext count and dash vaiable to the element, used += after the innertext, to leave a not delete the existing content

--- index.js
+++ index.js
@@ -9,19 +9,29 @@
 
 let countEl = document.getElementById("count-el");
 console.log(countEl);
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el");
 
 let count = 0;
 
 function increment() {
     count += 1;
     countEl.innerText = count;
-  console.log(count);
+  // console.log(count);
 }
 
 // 1. Create a function, save(), which logs out the count when it's called
 // const saveEl = document.getElementById("save");
 
+ // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+    // NB: Make sure to not delete the existing content of the paragraph
+
 function save() { 
+
+  let countAndDash = ` ${count} -`;
+  saveEl.innerText += countAndDash;
+
 
   console.log(count)
 }
