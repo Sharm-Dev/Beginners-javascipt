05e4e7b0afd0168218f3563c3855485483554af5
Sharm-Dev <sharmainetaylor@ymail.com>
2022-01-30 15:15:55 +0000

    Finished tasks to call functions

---
+++ Challenges/Task3/index.js
@@ -0,0 +1,16 @@
+let lapsCompleted = 0;
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+
+
+function addOne() {
+    lapsCompleted += 1;
+}
+
+addOne();
+addOne();
+addOne();
+
+console.log(lapsCompleted);
